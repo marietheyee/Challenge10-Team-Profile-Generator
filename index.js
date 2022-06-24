d0234186e96d0376a0ecc2bce146fc11cb90aede
Marie Yee <marieyee02@yahoo.com>
2022-06-24 11:48:37 -0700

    ran tests on indexjs

--- index.js
+++ index.js
@@ -119,3 +119,5 @@ function init() {
         addManagerProfile();
     });
 };
+
+init()
--- test/engineertest.js
+++
@@ -1,6 +0,0 @@
-const { default: test } = require("test");
-const Manager = require("./lib/Engineer");
-
-test("Set engineer's Github username.", () => {
-    const testGithubUsername = 'mar'
-})
