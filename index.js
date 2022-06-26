83aa0dbcc573135ee367f9c036d37b67372be100
Marie Yee <marieyee02@yahoo.com>
2022-06-25 20:15:19 -0700

    added more parameters in Engineer and Manager profiles

--- index.js
+++ index.js
@@ -1,31 +1,134 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const path = require("path");
 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const EmployeeQs = require("./lib/EmployeeQs");
+const ManagerQs = require("./lib/ManagerQs");
+const EngineerQs = require("./lib/EngineerQs");
+const InternQs = require("./lib/InternQs");
+const holdHtml = require("./lib/html-template");
 
-const render = require("./lib/htmlRenderer");
-const {run} = require("jest");
+let teamProfileArr = [];
 
-const employeeQuestions =[
-    {
-        type: "input",
-        message: "What's the employee's name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "What's the employee's ID?",
-        name: "id",
-    },
-    {
-        type: "input",
-        message: "what is the employee's email?",
-        name: "email",
-    }
-];
+const writeToFile = (holdHtmlArr) => {
+    return new Promise ((resolve, reject) => {
+        outputFileName = teamProfileArr[0].toLowerCase();
+        outputFileName = outputFileName.replace(/ /g, "-");
+
+        fs.writeFile(`./dist/${companyFileName}.html`, holdHtmlArr.join(""), function(err){
+            if (err) {
+                reject (err);
+                return;
+            };
+
+            resolve({
+                ok: true,
+                message: "File created. Your output HTML file is in the 'dist' folder."
+            });
+        });    
+    });
+};    
+
+function generateHtmlFile() {
+    const holdHtmlArr = holdHtml.generateHtml(teamProfileArr);
+    writeToFile(holdHtmlArr);
+};
+
+function addInternProfile() {
+    inquirer.prompt(InternQs)
+    .then (function(data) {
+        const internName = data.internName;
+        const internId = data.internId;
+        const internEmail = data.internEmail;
+        const internSchool = data.internSchool;
+        const teamMember = new Intern (internName, internId, internEmail, internSchool);
+
+        teamProfileArr.push(teamMember);
+
+        addNonManagerTeamMember();
+    });
+};
+
+function addEngineerProfile() {
+    inquirer.prompt(EngineerQs)
+    .then(function (data) {
+        const engineerName = data.engineerName;
+        const engineerId = data.engineerId;
+        const engineerEmail = data.engineerEmail;
+        const engineerGithubUsername = data.engineerGithubUsername;
+        const teamMember = new Engineer (engineerName, engineerId, engineerEmail, engineerGithubUsername);
+
+        teamProfileArr.push(teamMember)
+
+        addNonManagerTeamMember();
+
+    })
+};
+
+function addNonManagerTeamMember(){
+    console.log("add team members")
+    inquirer.prompt([
+        {
+            type: "list", 
+            message: "add team members",
+            name: "menuChoices",
+            choices: ["Add an Engineer Profile", "Add an Intern Profile", "Done Generating Profiles"]
+
+        }
+    ])
+    .then(function (data) {
+        switch (data.menuChoices) {
+            case "Add an Engineer Profile":
+                addEngineerProfile();
+                break;
+                case "Add an Intern Profile":
+                addInternProfile();
+                break;
+                case "Done Generating Profiles":
+                generateHtmlFile();
+                break;
+        };
+    })
+};
+
+function addManagerProfile() {
+    inquirer.prompt(ManagerQs)
+    .then(function(data) {
+        const managerName = data.managerName;
+        const managerId = data.managerId;
+        const managerEmail = data.managerEmail;
+        const managerOfficeNum = data.managerOfficeNum;
+        const teamMember = new Manager(managerName, managerId, managerEmail, managerOfficeNum);
+
+        teamProfileArr.push(teamMember);
+        
+        addNonManagerTeamMember()
+    });
+};
+
+function init() {
+    inquirer.prompt([
+        {
+            message: "Welcome to the Team Profile Generator. Please enter your team name:",
+            name: "teamName",
+            validate: teamNameInput => {
+                if (teamNameInput && teamNameInput.trim().length > 0) {
+                    return true;
+                } else {
+                    console.log("Enter your team name:");
+                    return false;
+                };
+            }
+        }
+    ])
+    .then(function (data) {
+        const teamName = data.teamName;
+        teamProfileArr.push(teamName);
+        addManagerProfile();
+    });
+};
+
+init()
